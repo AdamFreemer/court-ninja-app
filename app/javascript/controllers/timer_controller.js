af422f39ea2b8254f399506caef4314a33f7827f
Adam Freemer <adam@freemer.com>
2022-05-24 22:11:13 -0700

    Refactor a bit and add in rails data

--- app/javascript/controllers/timer_controller.js
+++ app/javascript/controllers/timer_controller.js
@@ -1,12 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = { time: Number, status: String }
+  static values = { tournamentTime: Number, breakTime: Number, status: String }
   static targets = [ "minute", "second" ]
 
   connect() {
     console.log("-- connect!")
-    console.log("-- this.timeValue: " + this.timeValue)
+    console.log("-- this.timeValue: " + this.tournamentTimeValue)
     console.log("-- this.statusValue: " + this.statusValue)
   }
 
@@ -26,19 +26,17 @@ export default class extends Controller {
   }
 
   timerRun() {
-    console.log("-- this.timeValue: " + this.timeValue)
+    console.log("-- this.timeValue: " + this.tournamentTimeValue)
     console.log("-- this.statusValue: " + this.statusValue)
-    this.timeValue--
+    this.tournamentTimeValue--
     this.generateTime();
   }
 
   generateTime() {
-    var second = this.timeValue % 60;
-    var minute = Math.floor(this.timeValue / 60) % 60;
-
+    var second = this.tournamentTimeValue % 60;
+    var minute = Math.floor(this.tournamentTimeValue / 60) % 60;
     second = (second < 10) ? '0'+ second : second;
     minute = (minute < 10) ? + minute : minute;
-    console.log('-- ' + minute + ":" + second)
     this.minuteTarget.innerHTML = minute
     this.secondTarget.innerHTML = second
   }
