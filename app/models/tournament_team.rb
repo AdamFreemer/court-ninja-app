204a1aa12d49a196b4985705eb7a14c71a4acfd4
Adam Freemer <adam@freemer.com>
2022-06-07 15:21:25 -0700

    Bug fix

--- app/models/tournament_team.rb
+++ app/models/tournament_team.rb
@@ -36,8 +36,7 @@ class TournamentTeam < ApplicationRecord
     teams_count_array = [*0..teams_count - 1].map(&:to_s)
     teams_count_array.each do |team_number|
       team = score_data[team_number]
-      team_lookup = Team.find_by(id: team['team_id'])
-      puts "== team #{team_number}"
+      team_lookup = TournamentTeam.find_by(id: team['team_id'])
       next if team_lookup.work_team == true
 
       team_lookup.update(score: team['score'])
