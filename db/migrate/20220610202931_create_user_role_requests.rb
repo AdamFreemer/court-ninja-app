0c7bed5b7d3406575ba0161ae39bae244dbd363c
Dan Brooking <dan.brooking@mysubscriptionaddiction.com>
2022-06-14 09:40:15 -0400

    Adding data migrations.

--- db/migrate/20220610202931_create_user_role_requests.rb
+++ db/migrate/20220610202931_create_user_role_requests.rb
@@ -1,5 +1,5 @@
 class CreateUserRoleRequests < ActiveRecord::Migration[7.0]
-  def change
+  def up
     create_table :user_role_requests do |t|
       t.references :user, foreign_key: true
       t.string :status, default: 'pending'
@@ -15,6 +15,17 @@ class CreateUserRoleRequests < ActiveRecord::Migration[7.0]
     end
 
     add_index(:user_role_requests_roles, [ :user_role_request_id, :role_id ], name: 'index_user_role_requests_roles_on_user_role_request_and_role')
+
+    ['coach', 'player', 'organization'].each do |role_name|
+      role = Role.find_or_create_by(name: role_name)
+      role.show_on_signup_form = true
+      role.save!
+    end
+  end
+
+  def down
+    drop_table :user_role_requests_roles
+    drop_table :user_role_requests
   end
 end
 
