1f449347e950d786cc4aa34c71a644086e197750
Dan Brooking <dan.brooking@mysubscriptionaddiction.com>
2022-06-24 18:26:31 -0400

    Add logged in user.

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'registrations' }
 
-  resources :users
+  resources :users, only: [:show, :edit, :update]
   resources :teams, except: :index do
     collection do
       get 'verify'
