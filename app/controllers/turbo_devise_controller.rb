10e3a82410a3579a9ba5311f6569ce267abd9cad
Dan Brooking <dan.brooking@mysubscriptionaddiction.com>
2022-07-05 16:37:47 -0400

    Show error message on login.

--- app/controllers/turbo_devise_controller.rb
+++ app/controllers/turbo_devise_controller.rb
@@ -1,13 +1,12 @@
 class TurboDeviseController < ApplicationController
- class Responder < ActionController::Responder
+  class Responder < ActionController::Responder
     def to_turbo_stream
       controller.render(options.merge(formats: :html))
-          rescue ActionView::MissingTemplate => error
-      if get?
-        raise error
-      elsif has_errors? && default_action
-        render rendering_options.merge(formats: :html,
-            status: :unprocessable_entity)
+    rescue ActionView::MissingTemplate => e
+      raise e if get?
+
+      if has_errors? && default_action
+        render rendering_options.merge(formats: :html, status: :unprocessable_entity)
       else
         redirect_to navigation_location
       end
