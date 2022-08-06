4afd682f8d56476526e538cca6e0a297dc7d85e6
Dan Brooking <dan.brooking@mysubscriptionaddiction.com>
2022-08-06 17:55:26 -0400

    Adding Sentry.

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,13 @@
+Sentry.init do |config|
+  config.dsn = 'https://332689e5c9cf40ec92686cfc850d5d4d@o1349690.ingest.sentry.io/6629300'
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |_context|
+    true
+  end
+end
