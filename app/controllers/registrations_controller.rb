753238c5788dd1ab1ec6e8b1c364c1f252d1d6d3
Dan Brooking <dmbrooking@gmail.com>
2022-08-18 07:18:29 -0400

    Show error message when signup fails.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,14 +9,17 @@ class RegistrationsController < Devise::RegistrationsController
     team = Team.find_by(invite_code: params['user']['invite_code']) if params['user']['invite_code'].present?
     resource.add_role :player if params['user']['role'] == 'Player' || team.present?
     resource.teams << team if team
-    resource.save!
 
-    return if team
+    if resource.save
+      return if team
 
-    return unless %w[Coach Organization].include?(params['user']['role'])
+      return unless %w[Coach Organization].include?(params['user']['role'])
 
-    req = UserRoleRequest.create!(user_id: resource.id)
-    req.add_role(params['user']['role'].downcase)
-    req.send_user_role_request_email
+      req = UserRoleRequest.create!(user_id: resource.id)
+      req.add_role(params['user']['role'].downcase)
+      req.send_user_role_request_email
+    else
+      redirect_to new_registration_path(@user) && return
+    end
   end
 end
