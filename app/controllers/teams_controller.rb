156b56d790ac933a566f1aaa35695674e13f9ac1
Dan Brooking <dmbrooking@gmail.com>
2022-10-01 16:27:12 -0400

    Add player to team (#88)
    * Coach add player, existing athlete can confirm.
    
    * New athletes working.
    
    * Change word on reset password screen to indicate claiming.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -55,10 +55,45 @@ class TeamsController < ApplicationController
     player = User.find(params[:player_id])
     team.players.delete(player)
 
+    redirect_back(fallback_location: root_path)
+  end
+
+  def add_player_to_team
+    team = Team.find(Integer(athlete_params[:team_id], 10))
+    user = User.find_by(email: athlete_params[:email])
+
+    if user
+      pt = PlayerTeam.create!(team: team, player: user, pending: true)
+      UserMailer.existing_athlete_join_team_approval_email(pt.id).deliver_now
+    else
+      pw = SecureRandom.uuid
+      user = User.create!(email: athlete_params[:email], first_name: athlete_params[:first_name], last_name: athlete_params[:last_name], password: pw, password_confirmation: pw)
+      pt = PlayerTeam.create!(team: team, player: user, pending: true)
+      UserMailer.new_athlete_join_team_approval_email(pt.id).deliver_now
+    end
+
+    redirect_back(fallback_location: root_path)
+  end
+
+  def existing_athlete_join_team_approval
+    pt = PlayerTeam.find_by(uuid: params[:uuid])
+
+    if pt.player == current_user
+      pt.pending = false
+      pt.save!
+      flash[:notice] = 'Successfully joined the team!'
+    else
+      flash[:error] = 'Error: Mismatch between logged in user and athlete associated with request'
+    end
+
     redirect_to root_path
   end
 
   private
+
+  def athlete_params
+    params.fetch(:user).permit(:first_name, :last_name, :email, :team_id)
+  end
 
   def team_params
     params.fetch(:team, [{}]).permit(:name, :description, :coach_id, :active)
--- app/models/player_team.rb
+++ app/models/player_team.rb
@@ -3,6 +3,8 @@
 # Table name: player_teams
 #
 #  id         :bigint           not null, primary key
+#  pending    :boolean          default(FALSE)
+#  uuid       :uuid             not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  player_id  :bigint
@@ -19,8 +21,16 @@
 #  fk_rails_...  (team_id => teams.id)
 #
 class PlayerTeam < ApplicationRecord
-  belongs_to :player, class_name: 'User'
+  belongs_to :player, class_name: 'User', optional: true
   belongs_to :team
 
   validates :team_id, uniqueness: { scope: :player_id }
+
+  def full_name
+    "#{pending_last_name}, #{pending_first_name}"
+  end
+
+  def initials
+    "#{pending_first_name[0]}#{pending_last_name[0]}"
+  end
 end
