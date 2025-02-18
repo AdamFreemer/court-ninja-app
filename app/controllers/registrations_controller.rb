e6164e2b485dd83e576c8f398721003ed4c085fa
Adam Freemer <adam@freemer.com>
2025-02-17 20:21:12 -0500

    Fix sign in bugs

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,34 +1,49 @@
 class RegistrationsController < Devise::RegistrationsController
-  skip_before_action :check_subscription  # Updated from check_if_subscribed
+  skip_before_action :check_subscription
+
   def new
     @invite_code = params[:invite_code]
     super
   end
 
-  # def after_sign_up_path_for(resource)
-  #   'https://buy.stripe.com/14k9Ezgq4bDa2kwfYZ', allow_other_host: true
-  # end
   def create
-    # team = Team.find_by(invite_code: params['user']['invite_code']) if params['user']['invite_code'].present?
-    # resource.add_role :athlete if params['user']['role'].downcase == 'athlete' || team.present?
-    # resource.teams << team if team
     recaptcha_valid = verify_recaptcha(model: @user)
-    if recaptcha_valid
-      super 
-      # set new user as coach
-      resource.is_coach = true
-      if resource.save
-        # return if team
-        # return unless %w[coach organization].include?(params['user']['role'].downcase)
-        req = UserRoleRequest.create!(user_id: resource.id)
-        # req.add_role(params['user']['role'].downcase)
-        req.send_user_request_email
-      else
-        redirect_to new_registration_path(@user), alert: 'Sign up error, please try again.' && return
-      end
-    else
+    
+    unless recaptcha_valid
       flash.delete(:recaptcha_error)
-      redirect_to new_user_registration_path, alert: 'Captcha error, please try again.'
+      redirect_to new_user_registration_path, 
+        alert: 'Captcha error, please try again.' and return
     end
+
+    build_resource(sign_up_params)
+
+    if User.exists?(email: resource.email)
+      resource.errors.add(:email, 'is already taken')
+      render :new and return
+    end
+
+    if resource.password.present? && resource.password.length < 6
+      resource.errors.add(:password, 'must be at least 6 characters')
+      render :new and return
+    end
+
+    super do |resource|
+      if resource.persisted?
+        resource.is_coach = true
+        if resource.save
+          req = UserRoleRequest.create!(user_id: resource.id)
+          req.send_user_request_email
+        end
+      end
+    end
+  end
+
+  protected
+
+  def after_sign_up_path_for(resource)
+    {
+      url: 'https://buy.stripe.com/14k9Ezgq4bDa2kwfYZ',
+      allow_other_host: true 
+    }
   end
 end
